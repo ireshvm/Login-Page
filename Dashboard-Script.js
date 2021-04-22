a0c2c2cb2932b325284e11e2413de16c8a6ceb81
Git-Harshit <askharshitgupta@gmail.com>
2021-04-22 22:04:21 +0530

    Add New Device Popup Form in Dashboard

---
+++ Dashboard-Script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	// Initialising the side navbar (for mobile screens)
+	$('.sidenav').sidenav();
+	// Initialising the 'Add New Device' modal 
+	$('.modal').modal();
+	// Initialising select options 
+	$('select').formSelect();
+});
