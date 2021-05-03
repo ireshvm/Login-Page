d6d43108380e506f2b6d6fd69c3605c219e75f0c
Git-Harshit <askharshitgupta@gmail.com>
2021-05-03 20:10:11 +0530

    Add My Profile Page

--- js/Dashboard-Script.js
+++ js/Dashboard-Script.js
@@ -3,6 +3,20 @@ $(document).ready(function(){
 	$('.sidenav').sidenav();
 	// Initialising the 'Add New Device' modal 
 	$('.modal').modal();
+	// Initialising tooltips (currently used in COPY Option)
+	$('.tooltipped').tooltip();
 	// Initialising select options 
 	$('select').formSelect();
+});
+
+// Setting COPY functionality
+$("div.card-action > a.tooltipped").on('hover', function() {
+	// $('span.key')[0]
+	console.log($(this));
+	$(this).attr('data-tooltip', 'Click to Copy API Key to clipboard');
+});
+$("div.card-action > a.tooltipped").on('click', ()=>{
+	// $('span.key')[0]
+	$(this).attr('data-tooltip', 'Copied!');
+	document.execCommand('copy');
 });
